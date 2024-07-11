a1827f316b31af150f33d74a837a56603a0ef7b2
Shady <shady.ber9@gmail.com>
2024-07-11 22:24:20 +0300

    images
    images

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -9,10 +9,10 @@
                                     <div class="pl-10 text-left single-slider-content light-content" style="text-align: left !important;">
                                         <div class="slider-heading small-heading-text" data-animation="flipInX" data-delay=".4s">
                                             <h2 class="mb-0"></h2>
-                                            <h2 class="mb-0">New Trending 2019</h2>
+                                            <h2 class="mb-0">New Trending {{ date('Y') }}</h2>
                                         </div>
                                         <div class="slider-desc" data-animation="flipInX" data-delay=".6s">
-                                            <p class="mb-0 mt-35">New Spring drops from Over. Shop the Collection</p>
+                                            <p class="mb-0 mt-35">New item Collections only for you.</p>
                                         </div>
                                         <div class="slider-link" data-animation="flipInX" data-delay=".8s">
                                             <a href="/shop" class="generic-btn mt-70 red-hover-btn text-uppercase" tabindex="0">Discover now</a>
@@ -26,11 +26,11 @@
                                 <div class="single-slider-inner">
                                     <div class="pl-10 text-left single-slider-content light-content" style="text-align: left !important;">
                                         <div class="slider-heading small-heading-text" data-animation="flipInX" data-delay=".4s">
-                                            <h2 class="mb-0">Wood Monkey</h2>
-                                            <h2 class="mb-0">New Arrivals 2019</h2>
+                                            <h2 class="mb-0">Virtual Assets and Game Coins</h2>
+                                            <h2 class="mb-0"> Game Coins and Software </h2>
                                         </div>
                                         <div class="slider-desc" data-animation="flipInX" data-delay=".6s">
-                                            <p class="mb-0 mt-35">New Spring drops from Over. Shop the Collection</p>
+                                            <p class="mb-0 mt-35">Most Trusted Site In Addis </p>
                                         </div>
                                         <div class="slider-link" data-animation="flipInX" data-delay=".8s">
                                             <a href="/shop" class="generic-btn mt-70 red-hover-btn text-uppercase" tabindex="0">Discover now</a>
@@ -44,11 +44,11 @@
                                 <div class="single-slider-inner">
                                     <div class="pl-10 text-left single-slider-content light-content" style="text-align: left !important;">
                                         <div class="slider-heading small-heading-text" data-animation="flipInX" data-delay=".4s">
-                                            <h2 class="mb-0">Free Size</h2>
-                                            <h2 class="mb-0">Sweater Woman's</h2>
+                                            <h2 class="mb-0">Verities of Items </h2>
+                                            <h2 class="mb-0"> Find What You Want Here</h2>
                                         </div>
                                         <div class="slider-desc" data-animation="flipInX" data-delay=".6s">
-                                            <p class="mb-0 mt-35">New Spring drops from Over. Shop the Collection</p>
+                                            <p class="mb-0 mt-35">New way to shop what you want.</p>
                                         </div>
                                         <div class="slider-link" data-animation="flipInX" data-delay=".8s">
                                             <a href="/shop" class="generic-btn mt-70 red-hover-btn text-uppercase" tabindex="0">Discover now</a>
